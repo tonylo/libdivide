cd14ca21b686e05a16e39656a2f0be07a8d509b6
Tony Lofthouse <lofthous@amazon.com>
2014-01-06 13:32:14 -0800

    Build benchmark binary with and without idiv support
    Signed-off-by: Tony Lofthouse <lofthous@amazon.com>

--- android/Android.mk
+++ android/Android.mk
@@ -7,6 +7,11 @@ SRC := $(LOCAL_PATH)/..
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(SRC)/libdivide_benchmark.c
 LOCAL_MODULE := benchmark
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(SRC)/libdivide_benchmark.c
+LOCAL_MODULE := benchmark_idiv
 # Needed for platforms which support hardware integer divide
 LOCAL_CFLAGS += -mcpu=cortex-a15 -DARM_SUPPORT_IDIV
 include $(BUILD_EXECUTABLE)
